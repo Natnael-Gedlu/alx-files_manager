4cf6d7aecd9dbdd2d75d2786e34d60ef671c6477
Natnael-Gedlu <nategd800@gmail.com>
2024-05-15 12:03:21 +0300

    Files manager

---
+++ controllers/AppController.js
@@ -0,0 +1,31 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  /**
+   * Get the status of the Redis and MongoDB clients.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(), // Check if Redis client is alive
+      db: dbClient.isAlive(), // Check if MongoDB client is alive
+    });
+  }
+
+  /**
+   * Get the number of users and files in the database.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]) // Get counts of users and files
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount }); // Respond with the counts
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error.message }); // Handle potential errors
+      });
+  }
+}
---
+++ routes/index.js
@@ -0,0 +1,43 @@
+import { Express } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
+
+/**
+ * Function to inject routes into the Express application.
+ * @param {Express} api - The Express application instance.
+ */
+const injectRoutes = (api) => {
+  // AppController routes
+  api.get('/status', AppController.getStatus); // Route to get app status
+  api.get('/stats', AppController.getStats); // Route to get app stats
+
+  // AuthController routes
+  api.get('/connect', basicAuthenticate, AuthController.getConnect); // Route to connect user (login)
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect); // Route to disconnect user (logout)
+
+  // UsersController routes
+  api.post('/users', UsersController.postNew); // Route to create a new user
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe); // Route to get current user info
+
+  // FilesController routes
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload); // Route to upload a file
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow); // Route to get file info by ID
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex); // Route to list all files
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish); // Route to publish a file
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish); // Route to unpublish a file
+  api.get('/files/:id/data', FilesController.getFile); // Route to get file data by ID
+
+  // Default route for handling unsupported routes
+  api.all('*', (req, res, next) => {
+    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
+  });
+
+  // Middleware for handling errors
+  api.use(errorResponse);
+};
+
+export default injectRoutes;
